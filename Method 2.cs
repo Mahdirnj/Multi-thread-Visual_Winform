d296769003b89c4316dce948d504b124cfb02417
Mahdi Ranjkesh <mahdiranjkesh16@gmail.com>
2023-03-10 20:12:03 +0330

    Forms and nav has been created

---
+++ Method 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_thread_Visual
+{
+    public partial class Method_2 : Form
+    {
+        public Method_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
